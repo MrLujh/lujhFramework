5aa6957ea6947e89b614ab6d89a499d602b867b8
daniulaolu <287929070@qq.com>
2018-03-14 14:47:02 +0800

    添加工程文件

---
+++ tesetDemo/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  tesetDemo
+//
+//  Created by lujh on 2018/3/14.
+//  Copyright © 2018年 lujh. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Framework/lujh.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    Pepole *p = [[Pepole alloc] init];
+    [p run];
+}
+
+
+@end
---
+++ tesetDemo/lujhFramework/Framework.framework/Headers/lujh.h
@@ -0,0 +1,14 @@
+//
+//  lujh.h
+//  Framework
+//
+//  Created by lujh on 2018/3/14.
+//  Copyright © 2018年 lujh. All rights reserved.
+//
+
+#ifndef lujh_h
+#define lujh_h
+
+#import "Pepole.h"
+
+#endif /* lujh_h */
